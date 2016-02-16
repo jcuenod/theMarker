jcuenod/theMarker

commit 84d01b11c2a78f92ca22321a18ab2b47c4bb77ff

Display frequencyCount

js/themarker.js
@@ -486,9 +486,10 @@ function scrollToWord(word, offset)
 
 function showDefinition(lemma)
 {
-	var $title = $("<div>").addClass("definitionTitle").text(lemma);
+	var dictionaryEntry = dictionaryData[normalizePolytonicGreekToLowerCase(lemmaWithoutBrackets)];
+	var $title = $("<div>").addClass("definitionTitle").text(lemma + "(" + dictionaryEntry.frequencyCount + ")");
 	var lemmaWithoutBrackets = lemma.replace(/\(.+?\)/g, '');
-	var lemmaDefinition = dictionaryData[normalizePolytonicGreekToLowerCase(lemmaWithoutBrackets)].definition;
+	var lemmaDefinition = dictionaryEntry.definition;
 	var $msg = $("<p>");
 	lemmaDefinition.split(/([\u0370-\u03FF\u1F00-\u1FFF]+)/).forEach(function(l){
 		if (l.match(/[\u0370-\u03FF\u1F00-\u1FFF]+/))

commit dd70f8032e9a4433ba82a5000a6e7f827e4b24f1

Fixed regression from 84d01b11c2a78f92ca22321a18ab2b47c4bb77ff

js/themarker.js
@@ -486,9 +486,9 @@ function scrollToWord(word, offset)
 
 function showDefinition(lemma)
 {
+	var lemmaWithoutBrackets = lemma.replace(/\(.+?\)/g, '');
 	var dictionaryEntry = dictionaryData[normalizePolytonicGreekToLowerCase(lemmaWithoutBrackets)];
 	var $title = $("<div>").addClass("definitionTitle").text(lemma + "(" + dictionaryEntry.frequencyCount + "x)");
-	var lemmaWithoutBrackets = lemma.replace(/\(.+?\)/g, '');
 	var lemmaDefinition = dictionaryEntry.definition;
 	var $msg = $("<p>");
 	lemmaDefinition.split(/([\u0370-\u03FF\u1F00-\u1FFF]+)/).forEach(function(l){